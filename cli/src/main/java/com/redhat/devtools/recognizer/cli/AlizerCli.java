83e9410fdee4f1df01d3d6da3d72b48ce2e722d3
Jeff MAURY <jmaury@redhat.com>
2021-04-01 19:21:26 +0200

    Add new module to load devfile metadata from registries
    Adapt cli accordingly
    
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- cli/src/main/java/com/redhat/devtools/recognizer/cli/AlizerCli.java
+++ cli/src/main/java/com/redhat/devtools/recognizer/cli/AlizerCli.java
@@ -10,13 +10,19 @@
  ******************************************************************************/
 package com.redhat.devtools.recognizer.cli;
 
+import com.redhat.devtools.alizer.registry.support.DevfileMetadata;
+import com.redhat.devtools.alizer.registry.support.DevfileRegistryMetadataProviderBuilder;
 import com.redhat.devtools.recognizer.api.DevfileType;
+import com.redhat.devtools.recognizer.api.Language;
+import com.redhat.devtools.recognizer.api.LanguageRecognizer;
 import com.redhat.devtools.recognizer.api.LanguageRecognizerBuilder;
 import io.quarkus.picocli.runtime.annotations.TopCommand;
 import java.io.File;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+
 import picocli.CommandLine;
 
 @TopCommand
@@ -29,12 +35,16 @@ public class AlizerCli implements Runnable{
     @CommandLine.Parameters(index = "0", description = "The project to analyze")
     String name;
 
+    @CommandLine.Option(names = {"-r", "--registry"}, description = "Pass the registry")
+    List<String> registries;
+
     @Override
     public void run() {
         if (name == null) return;
+        LanguageRecognizer reco = new LanguageRecognizerBuilder().build();
 
             try {
-                new LanguageRecognizerBuilder().build().analyze(name).forEach(
+                reco.analyze(name).forEach(
                         lang -> {
                             System.out.printf("%-10s %-10s %-10s %-10s\n",
                                     lang.getName(),
@@ -43,6 +53,15 @@ public class AlizerCli implements Runnable{
                                     String.format(" % .2f", lang.getUsageInPercentage()) + "%");
                         }
                 );
+                if (registries != null && !registries.isEmpty()) {
+                    List<DevfileMetadata> devfiles = new DevfileRegistryMetadataProviderBuilder().withURLs(registries).build().getDevfileMetada();
+                    DevfileType type = reco.selectDevFileFromTypes(name,devfiles.stream().map(it -> new DevfileTypeAdapter(it)).collect(Collectors.toList()));
+                    if (type != null) {
+                        System.out.println("Devfile " + type.getName() + " matched");
+                    } else {
+                        System.out.println("No devfile found to match");
+                    }
+                }
             } catch (IOException e) {}
         }
 }
