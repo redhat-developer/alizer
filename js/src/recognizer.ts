c917809eb8f272c6f50738a669582c7da9a1401f
Luca Stocchi <lstocchi@redhat.com>
2021-07-20 13:02:18 +0200

    feat: add devfile selection in js impl (#33)
    Signed-off-by: Luca Stocchi <lstocchi@redhat.com>

--- js/src/recognizer.ts
+++ js/src/recognizer.ts
@@ -7,21 +7,68 @@ import * as jsyaml from 'js-yaml';
 import {promises as fs} from 'fs';
 import * as path from 'path';
 import { glob } from 'glob-gitignore';
-import { Language } from './types';
-import { JAVA, PYTHON } from './constants';
+import { DevfileType, Language } from './types';
+import { FRAMEWORK_WEIGHT, JAVA, PYTHON, TOOL_WEIGHT } from './constants';
 import { getJava } from './javaRecognizer';
 import { getPython } from './pythonRecognizer';
+
+
+export async function selectDevFileFromTypes(root: string, devfileTypes: DevfileType[]) {
+    const languages = await detectLanguages(root);
+    for (const language of languages) {
+        const devfileTypesPickedUsingLanguage = devfileTypes
+                        .map(devfileType => { 
+                            return {
+                                score: computeLanguageScore(language, devfileType),
+                                devfileType
+                            };
+                        })
+                        .filter(devfileType => devfileType.score > 0)
+                        .sort((a, b) => b.score - a.score);
+        if (devfileTypesPickedUsingLanguage.length > 0) {
+            return devfileTypesPickedUsingLanguage[0].devfileType;
+        }
+    }
+    return;
+}
+
+function computeLanguageScore(language: Language, devfileType: DevfileType) {
+    let score = 0;
+    if (devfileType.getLanguage() === language.name || matches(devfileType.getLanguage(), language.aliases)) {
+        score++;
+        if (devfileType.getProjectType() && matches(devfileType.getProjectType(), language.frameworks)) {
+            score += FRAMEWORK_WEIGHT;
+        }
+        const tags = devfileType.getTags();
+        if (tags && tags.length > 0) {
+            tags.forEach(tag => {
+                if (matches(tag, language.frameworks)) {
+                    score += FRAMEWORK_WEIGHT;
+                }
+                if (language.builder === tag) {
+                    score += TOOL_WEIGHT;
+                }
+            });
+        }
+    }
+    return score;
+}
+
+function matches(value: string, list?: string[]) {
+    return list && list.filter(s => s.toLowerCase() === value.toLowerCase()).length > 0;
+}
 
 export async function detectLanguages(root: string): Promise<Language[]> {
     if (!root) {
         throw new Error('The project root is not valid');
     }
-    const fileWithLanguages = await fs.readFile(path.join(__dirname, '..', 'resources', 'languages.yaml'), 'utf-8');
-    const allLanguages = jsyaml.safeLoad(fileWithLanguages);
     
-    const files = await getFiles(root);
+    const YAMLFileWithAllLanguages = await fs.readFile(path.join(__dirname, '..', 'resources', 'languages.yaml'), 'utf-8');
+    const allLanguages = jsyaml.safeLoad(YAMLFileWithAllLanguages);
+    
+    const allFilesFromRoot = await getFiles(root);
 
-    const extensionOccurrences = files.reduce((acc, curr) => {
+    const extensionOccurrences = allFilesFromRoot.reduce((acc, curr) => {
         curr = `.${curr.split('.').pop()}`;
         if (!acc[curr]) {
             acc[curr] = 1;
@@ -37,21 +84,24 @@ export async function detectLanguages(root: string): Promise<Language[]> {
         if (attributes.hasOwnProperty('type') && 
             attributes.hasOwnProperty('extensions') &&
             (attributes as any).type === 'programming') {
-            let languageOccurrence = (attributes as any).extensions.
-                                filter(element => Object.keys(extensionOccurrences).indexOf(element) !== -1).
-                                map(item => extensionOccurrences[item])[0];
-            if (languageOccurrence) {
-                if (languagesFiltered.hasOwnProperty(language)) {
-                    languageOccurrence += languagesFiltered[language];
+            let languageOccurrence = (attributes as any).extensions
+                                .filter(element => Object.keys(extensionOccurrences).indexOf(element) !== -1)
+                                .reduce((acc, curr) => acc + extensionOccurrences[curr], 0);
+            if (languageOccurrence !== 0) {
+                languagesFiltered[language] = {
+                    aliases: (attributes as any).aliases,
+                    occurrence: languageOccurrence
                 }
-                languagesFiltered[language] = languageOccurrence;
             }            
         }
     }
 
     const languagesDetected: Language[] = [];
     for(const language of Object.entries(languagesFiltered).sort(([_k1, v1], [_k2, v2]) => (v2 as number)-(v1 as number))) {
-        languagesDetected.push(await getDetailedLanguage(language[0], files));
+        languagesDetected.push({
+            aliases: (language[1] as any).aliases,
+            ...await getDetailedLanguage(language[0], allFilesFromRoot)
+        });
     }
     return languagesDetected;
     
