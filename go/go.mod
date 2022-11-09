ddc6208ba0077a8e446e88e36b9f5f02e49b74b2
Luca Stocchi <lstocchi@redhat.com>
2022-11-09 15:25:12 +0100

    chore: run alizer test on external git repos (#107)
    Signed-off-by: Luca Stocchi <lstocchi@redhat.com>

--- go/go.mod
+++ go/go.mod
@@ -3,6 +3,7 @@ module github.com/redhat-developer/alizer/go
 go 1.17
 
 require (
+	github.com/go-git/go-git/v5 v5.4.2
 	github.com/moby/buildkit v0.10.3
 	github.com/sabhiram/go-gitignore v0.0.0-20210923224102-525f6e181f06
 	github.com/spf13/cobra v1.5.0
@@ -12,11 +13,28 @@ require (
 )
 
 require (
+	github.com/Microsoft/go-winio v0.5.1 // indirect
+	github.com/ProtonMail/go-crypto v0.0.0-20210428141323-04723f9f07d7 // indirect
+	github.com/acomagu/bufpipe v1.0.3 // indirect
 	github.com/containerd/typeurl v1.0.2 // indirect
+	github.com/emirpasic/gods v1.12.0 // indirect
+	github.com/go-git/gcfg v1.5.0 // indirect
+	github.com/go-git/go-billy/v5 v5.3.1 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/imdario/mergo v0.3.12 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
+	github.com/kevinburke/ssh_config v0.0.0-20201106050909-4977a11b4351 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sergi/go-diff v1.1.0 // indirect
+	github.com/xanzy/ssh-agent v0.3.0 // indirect
+	golang.org/x/crypto v0.0.0-20211202192323-5770296d904e // indirect
+	golang.org/x/net v0.0.0-20211216030914-fe4d6282115f // indirect
+	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
+	golang.org/x/term v0.0.0-20210615171337-6886f2dfbf5b // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/warnings.v0 v0.1.2 // indirect
 )
---
+++ go/test/apis/git_recognizer_test.go
@@ -0,0 +1,126 @@
+package recognizer
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/redhat-developer/alizer/go/pkg/apis/model"
+	"github.com/redhat-developer/alizer/go/pkg/utils"
+	"github.com/redhat-developer/alizer/go/test"
+)
+
+func TestExternalRepos(t *testing.T) {
+	// read git test file to retrieve all git repos and their expected properties
+	jsonFile, err := ioutil.ReadFile("../git_test.json")
+	if err != nil {
+		t.Fatal("Unable to fetch git repositories file to run tests to")
+	}
+	var data test.GitTests
+	err = json.Unmarshal(jsonFile, &data)
+	if err != nil {
+		t.Fatal("Unable to fetch git repositories file to run tests to")
+	}
+
+	// loop over all repositories and verify expected results are correct
+	for repo, properties := range data {
+		dir, err := test.CheckoutCommit(repo, properties.Commit)
+		defer os.RemoveAll(dir)
+		if err != nil {
+			t.Errorf("Unable to download git repo %s", err.Error())
+		} else {
+			assertComponentsBelongToGitProject(t, dir, properties.Components)
+		}
+	}
+
+}
+
+func assertComponentsBelongToGitProject(t *testing.T, gitProjectPath string, expectedComponents []test.ComponentProperties) {
+	components := getComponentsFromProjectInner(t, gitProjectPath)
+	assertNumberOfComponents := len(components) == len(expectedComponents)
+	if assertNumberOfComponents {
+		// sort both slices by component name
+		sort.Slice(components, func(i, j int) bool {
+			return strings.ToLower(components[i].Name) < strings.ToLower(components[j].Name)
+		})
+		sort.Slice(expectedComponents, func(i, j int) bool {
+			return strings.ToLower(expectedComponents[i].Name) < strings.ToLower(expectedComponents[j].Name)
+		})
+
+		cont := 0
+		for cont < len(components) {
+			if !strings.EqualFold(components[cont].Name, expectedComponents[cont].Name) {
+				t.Errorf("Expected to find component %s but it was found %s", expectedComponents[cont].Name, components[cont].Name)
+			}
+			if !assertExpectedLangsAreFound(expectedComponents[cont].Languages, components[cont].Languages) {
+				expectedPretty := printPrettyStruct(expectedComponents[cont].Languages)
+				foundPretty := printPrettyStruct(components[cont].Languages)
+				t.Errorf("Languages found are different from those expected.\nExpected: %s\nFound: %s ", expectedPretty, foundPretty)
+			}
+			if !assertExpectedPortsAreFound(expectedComponents[cont].Ports, components[cont].Ports) {
+				expectedPretty := printPrettyStruct(expectedComponents[cont].Ports)
+				foundPretty := printPrettyStruct(components[cont].Ports)
+				t.Errorf("Languages found are different from those expected.\nExpected: %s\nFound: %s ", expectedPretty, foundPretty)
+			}
+			cont++
+		}
+	} else {
+		t.Errorf("Expected " + strconv.Itoa(len(expectedComponents)) + " components but they were " + strconv.Itoa(len(components)))
+	}
+}
+
+func printPrettyStruct(v interface{}) string {
+	pretty, _ := json.MarshalIndent(v, "", "\t")
+	return string(pretty)
+}
+
+func assertExpectedLangsAreFound(expectedLangs []test.ComponentLanguage, foundLangs []model.Language) bool {
+	if !strings.EqualFold(expectedLangs[0].Name, foundLangs[0].Name) {
+		return false //the main language is different
+	}
+	for _, expectedLang := range expectedLangs {
+		lang := func() model.Language {
+			for _, foundLang := range foundLangs {
+				if strings.EqualFold(expectedLang.Name, foundLang.Name) {
+					return foundLang
+				}
+			}
+			return model.Language{}
+		}()
+		if lang.Name == "" {
+			return false
+		}
+		if !assertExpectedAreFound(expectedLang.Frameworks, lang.Frameworks) {
+			return false
+		}
+		if !assertExpectedAreFound(expectedLang.Tools, lang.Tools) {
+			return false
+		}
+	}
+	return true
+}
+
+func assertExpectedPortsAreFound(expectedPorts []int, foundPorts []int) bool {
+	return assertExpectedAreFound(intSliceToStringSlice(expectedPorts), intSliceToStringSlice(foundPorts))
+}
+
+func intSliceToStringSlice(ints []int) []string {
+	var sliceString []string
+	for _, val := range ints {
+		sliceString = append(sliceString, strconv.Itoa(val))
+	}
+	return sliceString
+}
+
+func assertExpectedAreFound(expected []string, found []string) bool {
+	for i := 0; i < len(expected); i++ {
+		if !utils.Contains(found, expected[i]) {
+			return false
+		}
+	}
+	return true
+}
