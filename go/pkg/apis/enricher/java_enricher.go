6c69cc22ed1f6a7b094e0e16e9fba76e77aecae3
thepetk <thepetk@gmail.com>
2023-03-29 16:57:04 +0100

    Add wildfly detector to java enricher
    Signed-off-by: thepetk <thepetk@gmail.com>

--- go/pkg/apis/enricher/java_enricher.go
+++ go/pkg/apis/enricher/java_enricher.go
@@ -31,6 +31,7 @@ func getJavaFrameworkDetectors() []FrameworkDetectorWithConfigFile {
 		&framework.QuarkusDetector{},
 		&framework.SpringDetector{},
 		&framework.VertxDetector{},
+		&framework.WildFlyDetector{},
 	}
 }
 
@@ -132,11 +133,14 @@ func (j JavaEnricher) IsConfigValidForComponentDetection(language string, config
 }
 
 /*
-	isParentModuleMaven checks if configuration file is a parent pom.xml
-	Parameters:
-		configPath: configuration file path
-	Returns:
-		bool: true if config file is parent
+isParentModuleMaven checks if configuration file is a parent pom.xml
+Parameters:
+
+	configPath: configuration file path
+
+Returns:
+
+	bool: true if config file is parent
 */
 func isParentModuleMaven(configPath string) bool {
 	_, file := filepath.Split(configPath)
