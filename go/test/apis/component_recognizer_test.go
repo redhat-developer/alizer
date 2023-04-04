4ef6f0707b42e5536da6edc1576d41138395a085
thepetk <thepetk@gmail.com>
2023-04-04 13:25:13 +0100

    Fix go implementation tests
    Signed-off-by: thepetk <thepetk@gmail.com>

--- go/test/apis/component_recognizer_test.go
+++ go/test/apis/component_recognizer_test.go
@@ -144,7 +144,7 @@ func updateContent(filePath string, data []byte) error {
 
 func TestComponentDetectionMultiProjects(t *testing.T) {
 	components := getComponentsFromProject(t, "")
-	nComps := 19
+	nComps := 20
 	if len(components) != nComps {
 		t.Errorf("Expected " + strconv.Itoa(nComps) + " components but found " + strconv.Itoa(len(components)))
 	}
