68ca7d5c753fd0333088560266f29fd43396f2f8
thepetk <thepetk@gmail.com>
2023-04-11 14:53:53 +0100

    Remove port detection from JBoss and Wildfly detectors
    Signed-off-by: thepetk <thepetk@gmail.com>

--- go/pkg/apis/enricher/framework/java/wildfly_detector.go
+++ go/pkg/apis/enricher/framework/java/wildfly_detector.go
@@ -12,23 +12,11 @@ package enricher
 
 import (
 	"context"
-	"encoding/xml"
-	"strings"
 
 	"github.com/redhat-developer/alizer/go/pkg/apis/model"
-	utils "github.com/redhat-developer/alizer/go/pkg/utils"
 )
 
 type WildFlyDetector struct{}
-
-type Wildfly_Standalone_Xml struct {
-	SocketBindingGroup struct {
-		SocketBinding []struct {
-			Name string `xml:"name,attr"`
-			Port string `xml:"port,attr"`
-		} `xml:"socket-binding"`
-	} `xml:"socket-binding-group"`
-}
 
 func (o WildFlyDetector) GetSupportedFrameworks() []string {
 	return []string{"WildFly"}
@@ -41,32 +29,5 @@ func (o WildFlyDetector) DoFrameworkDetection(language *model.Language, config s
 }
 
 func (o WildFlyDetector) DoPortsDetection(component *model.Component, ctx *context.Context) {
-	bytes, err := utils.ReadAnyApplicationFile(component.Path, []model.ApplicationFileInfo{
-		{
-			Dir:  "",
-			File: "standalone.xml",
-		},
-	}, ctx)
-	if err != nil {
-		return
-	}
-	var (
-		data       Wildfly_Standalone_Xml
-		foundPorts []string
-	)
-	xml.Unmarshal(bytes, &data)
-	filters := [3]string{"${jboss.http.port:", "${jboss.https.port:", "}"}
-	for _, potentialPort := range data.SocketBindingGroup.SocketBinding {
-		if potentialPort.Name == "https" || potentialPort.Name == "http" {
-			tmpPort := potentialPort.Port
-			for _, filter := range filters {
-				tmpPort = strings.ReplaceAll(tmpPort, filter, "")
-			}
-			foundPorts = append(foundPorts, tmpPort)
-		}
-	}
-	ports := utils.GetValidPorts(foundPorts)
-	if len(ports) > 0 {
-		component.Ports = ports
-	}
+	// Not implemented
 }
