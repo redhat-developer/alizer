367a8d2975f77ead82ce7fd8cd4bf8aea7ab4238
thepetk <thepetk@gmail.com>
2023-04-18 10:05:07 +0100

    Remove unnecessary comments on test project
    Signed-off-by: thepetk <thepetk@gmail.com>

--- resources/projectPortTesting/projectGo/main.go
+++ resources/projectPortTesting/projectGo/main.go
@@ -7,11 +7,7 @@ import (
 )
 
 func main() {
-
-	//api := mux.NewRouter()
 	http.HandleFunc("/", HelloHandler)
-	//http.Handle("/hello", api)
-
 	fmt.Println("Listening on localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
@@ -22,7 +18,5 @@ func HelloHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Println("unable to get hostname")
 	}
-
-	// fmt.Fprintf(w, "Hello from Go! %s on %s\n", time.Now(), hostname)
 	fmt.Fprintf(w, "Go Hello on %s\n", hostname)
 }
