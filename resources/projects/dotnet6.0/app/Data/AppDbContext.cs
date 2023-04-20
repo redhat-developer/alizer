9926b0e0645aa7ef65011f0dc7fe0ef7baea7f3c
thepetk <thepetk@gmail.com>
2023-04-20 12:14:59 +0100

    Add test resources for dotnet
    Signed-off-by: thepetk <thepetk@gmail.com>

---
+++ resources/projects/dotnet6.0/app/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RazorPagesContacts.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Customer> Customers { get; set; }
+}
