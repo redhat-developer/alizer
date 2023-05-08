154e4bfdabacb14e7e580db54538f40951184301
thepetk <thepetk@gmail.com>
2023-05-08 16:29:27 +0100

    Update sum of tests
    Signed-off-by: thepetk <thepetk@gmail.com>

--- go/test/apis/component_recognizer_test.go
+++ go/test/apis/component_recognizer_test.go
@@ -168,7 +168,7 @@ func updateContent(filePath string, data []byte) error {
 
 func TestComponentDetectionMultiProjects(t *testing.T) {
 	components := getComponentsFromProject(t, "")
-	nComps := 31
+	nComps := 29
 	if len(components) != nComps {
 		t.Errorf("Expected " + strconv.Itoa(nComps) + " components but found " + strconv.Itoa(len(components)))
 	}
