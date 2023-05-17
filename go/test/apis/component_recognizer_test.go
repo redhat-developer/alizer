3ba668951e7de17a09aeb857e90106a2c8e1e84e
Michael Hoang <mhoang@redhat.com>
2023-05-17 09:41:32 -0400

    updating test
    Signed-off-by: Michael Hoang <mhoang@redhat.com>

--- go/test/apis/component_recognizer_test.go
+++ go/test/apis/component_recognizer_test.go
@@ -172,7 +172,7 @@ func updateContent(filePath string, data []byte) error {
 
 func TestComponentDetectionMultiProjects(t *testing.T) {
 	components := getComponentsFromProject(t, "")
-	nComps := 31
+	nComps := 32
 	if len(components) != nComps {
 		t.Errorf("Expected " + strconv.Itoa(nComps) + " components but found " + strconv.Itoa(len(components)))
 	}
