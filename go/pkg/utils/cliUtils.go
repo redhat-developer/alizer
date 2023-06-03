1b94ca77e7c29cbe3c43d6cbf99d4be04ee907d8
thepetk <thepetk@gmail.com>
2023-06-03 20:24:54 +0100

    Implement logger in utils
    Signed-off-by: thepetk <thepetk@gmail.com>

--- go/pkg/utils/cliUtils.go
+++ go/pkg/utils/cliUtils.go
@@ -4,6 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+
+	"github.com/go-logr/logr"
+	"go.uber.org/zap/zapcore"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 )
 
 func PrintNoArgsWarningMessage(command string) {
@@ -12,6 +16,17 @@ No arg received. Did you forget to add the project path to analyze?
 
 Expected:
   alizer %s /your/local/project/path [flags]
+
+To find out more, run 'alizer %s --help'
+`, command, command)
+}
+
+func PrintWrongLoggingLevelMessage(command string) {
+	fmt.Printf(`
+Argument log has wrong value. Did you choose one of debug, warning, info?
+
+Expected:
+  alizer %s /your/local/project/path --log [debug, warning, info]
 
 To find out more, run 'alizer %s --help'
 `, command, command)
@@ -35,4 +50,33 @@ func RedirectErrorToStdErrAndExit(err error) {
 func RedirectErrorStringToStdErrAndExit(err string) {
 	fmt.Fprintln(os.Stderr, err)
 	os.Exit(1)
+}
+
+var Logger logr.Logger
+
+func getZapcoreLevel(level string) (zapcore.Level, error) {
+	switch level {
+	case "debug":
+		return zapcore.DebugLevel, nil
+	case "warning":
+		return zapcore.WarnLevel, nil
+	case "info":
+		return zapcore.InfoLevel, nil
+	case "":
+		return zapcore.ErrorLevel, nil
+	default:
+		return nil, fmt.Errorf("log leve %s does not exist", level)
+	}
+}
+
+func GenLogger(logLevel string) error {
+	level, err := getZapcoreLevel(logLevel)
+	if err != nil {
+		return err
+	}
+	Logger = zap.New(zap.UseFlagOptions(&zap.Options{
+		Development: true,
+		TimeEncoder: zapcore.ISO8601TimeEncoder,
+		Level:       level,
+	}))
 }
