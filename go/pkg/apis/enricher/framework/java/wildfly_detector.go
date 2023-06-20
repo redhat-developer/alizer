7ee160d05b4e13d598b98c8b891fb86c34c2f648
thepetk <thepetk@gmail.com>
2023-06-20 18:06:35 +0100

    Update detection logic for wildfly
    Signed-off-by: thepetk <thepetk@gmail.com>

--- go/pkg/apis/enricher/framework/java/wildfly_detector.go
+++ go/pkg/apis/enricher/framework/java/wildfly_detector.go
@@ -13,9 +13,8 @@ package enricher
 
 import (
 	"context"
-	"os"
-	"path/filepath"
 	"regexp"
+	"strings"
 
 	"github.com/redhat-developer/alizer/go/pkg/apis/model"
 	"github.com/redhat-developer/alizer/go/pkg/utils"
@@ -34,34 +33,51 @@ func (o WildFlyDetector) DoFrameworkDetection(language *model.Language, config s
 	}
 }
 
-// DoPortsDetection for wildfly fetches the pom.xml and tries to find any packaging-script under
-// the wildfly-maven-plugin. If there is one it looks for a new-socket-binding where a port is defined.
+// DoPortsDetection for wildfly fetches the pom.xml and tries to find any javaOpts under
+// the wildfly-maven-plugin profiles. If there is one it looks if jboss.http.port is defined.
 func (o WildFlyDetector) DoPortsDetection(component *model.Component, ctx *context.Context) {
+	ports := []int{}
+	portPlaceholder := ""
+	// Fetch the content of xml for this component
 	paths, err := utils.GetCachedFilePathsFromRoot(component.Path, ctx)
 	if err != nil {
 		return
 	}
 	pomXML := utils.GetFile(&paths, "pom.xml")
-	if hasPkgScript, filePath := hasPackageScripts(pomXML, "org.wildfly.plugins", "wildfly-maven-plugin"); hasPkgScript {
-		configScript := utils.GetFile(&paths, filePath)
-		cleanConfigScript := filepath.Clean(configScript)
-		bytes, err := os.ReadFile(cleanConfigScript)
-		if err != nil {
-			return
+	pom, err := utils.GetPomFileContent(pomXML)
+	if err != nil {
+		return
+	}
+
+	re := regexp.MustCompile(`jboss.https?.port=\d*`)
+	// Check for port configuration inside profiles
+	for _, profile := range pom.Profiles.Profile {
+		for _, plugin := range profile.Build.Plugins.Plugin {
+			if !(strings.Contains(plugin.ArtifactId, "wildfly-maven-plugin") && strings.Contains(plugin.GroupId, "org.wildfly.plugins")) {
+				continue
+			}
+			matchIndexesSlice := re.FindAllStringSubmatchIndex(plugin.Configuration.JavaOpts, -1)
+			for _, matchIndexes := range matchIndexesSlice {
+				if len(matchIndexes) > 1 {
+					tempPortPlaceholder := plugin.Configuration.JavaOpts[matchIndexes[0]:matchIndexes[1]]
+					for _, httpArg := range []string{"jboss.http.port=", "jboss.https.port="} {
+						portPlaceholder = strings.Replace(tempPortPlaceholder, httpArg, "", -1)
+					}
+				}
+			}
 		}
-		matchIndexRegexes := []model.PortMatchRule{
-			{
-				Regex:     regexp.MustCompile(`new-socket-binding:add\([^)]*`),
-				ToReplace: "new-socket-binding:add",
-			},
-		}
-		if err != nil {
-			return
-		}
-		ports := utils.GetPortFromFilePackagingScript(matchIndexRegexes, string(bytes))
-		if len(ports) > 0 {
-			component.Ports = ports
-			return
-		}
+	}
+
+	if portPlaceholder == "" {
+		return
+	}
+
+	if port, err := utils.GetValidPort(portPlaceholder); err == nil {
+		ports = append(ports, port)
+	}
+
+	if len(ports) > 0 {
+		component.Ports = ports
+		return
 	}
 }
